65f720543a55841bb65c04735ed3f62b2c53281d
MatiasOrlando <matiassorlando@gmail.com>
2023-03-23 19:08:55 +0100

    feat: concurrently y next auth instalados, configuracion sass

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+import "../buildStyles/app.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
