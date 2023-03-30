42f5e39889985305a5251299e8e7e62aa71e43fe
NicholasInchauspe2 <inchauspenico@gmail.com>
2023-03-30 10:13:21 -0300

    userDataLogged

---
+++ api/config/validateMiddleware.js
@@ -0,0 +1,31 @@
+const User = require("../models/user")
+
+exports.isAdmin = async (req, res, next) => {
+try {
+    const user = await User.findById(req.params.id)
+    if(user.admin){
+      req.user = user
+      return next()
+    }
+    else{
+       return res.status(401).json({message: "Unathorized"})
+    }
+}catch(err){
+    return res.status(404).json({message: "Unknown Error"})
+}
+}
+
+exports.isOperator = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(user.operator){
+          req.user = user
+          return next()
+        }
+        else{
+            return res.status(401).json({message: "Unathorized"})
+        }
+    }catch(err){
+        return res.status(404).json({message: "Unknown Error"})
+    }
+    }
--- client/src/components/FormUseData/validation/validationUserData.js
+++ client/src/components/FormUseData/validation/validationUserData.js
@@ -2,12 +2,8 @@ import * as Yup from "yup";
 
 const validationUserData = {
     validationSchema: Yup.object({
-        name: Yup.string().required(),
         password: Yup.string().required(),
-        email: Yup.string().required(),
-        dni: Yup.number().required(),
-        phone: Yup.number().required(),
-    }),
+        }),
 };
 
 export default validationUserData;
