b46f91f0393b2603eb7f3dc8bda2da0c6d2ce472
MatiasOrlando <matiassorlando@gmail.com>
2023-03-31 20:21:28 +0200

    refresh user logeado arreglado

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,22 +1,20 @@
 import FormUserData from "@/components/FormUseData/FormUserData";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+
+export async function getServerSideProps(context) {
+  const sessionToken = context.req.cookies["next-auth.session-token"];
+  if (!sessionToken) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+}
 
 const user = () => {
-  const { data } = useSession();
-  const router = useRouter();
-  useEffect(() => {
-    if (!data && router.pathname !== "/") {
-      router.push("/");
-    }
-  }, [data, router]);
-
-  return (
-    <div>
-      <FormUserData />
-    </div>
-  );
+  return <FormUserData />;
 };
 
 export default user;
