75fd4c6d2bb648c0b59a7861a260c2ce4c1c7ea9
MatiasOrlando <matiassorlando@gmail.com>
2023-04-04 11:41:20 +0200

    feat: interactividad y actualizacion variables de estilos, vista cancelacion de resta

--- client/src/components/FormCancel/FormCancel.jsx
+++ client/src/components/FormCancel/FormCancel.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+
 const FormCancel = () => {
+  const [checkboxes, setCheckboxes] = useState([
+    { id: 1, isChecked: false, text: "Ya no quiero ir" },
+    { id: 2, isChecked: false, text: "Encontré un lugar mejor" },
+    { id: 3, isChecked: false, text: "Me equivoqué de horario" },
+    { id: 4, isChecked: false, text: "Me cancelaron" },
+    { id: 5, isChecked: false, text: "Otro" },
+  ]);
+
+  const handleCheckboxChange = (id) => {
+    const newCheckboxes = checkboxes.map((checkbox) => {
+      if (checkbox.id === id) {
+        return { ...checkbox, isChecked: !checkbox.isChecked };
+      } else {
+        return checkbox;
+      }
+    });
+    setCheckboxes(newCheckboxes);
+  };
+
   return (
     <>
       <div className="container-reason">
@@ -7,37 +28,39 @@ const FormCancel = () => {
           ¿Por qué desea cancelar su reserva?
         </div>
       </div>
-      <hr className="line-hr" />
-      <div className="no-show">
-        <input type="checkbox" name="" value="" className="checkbox-noshow" />
-        <p>Ya no quiero ir</p>
-      </div>
-      <hr className="line-hr" />
-      <div className="no-show">
-        <input type="checkbox" name="" value="" className="checkbox-noshow" />
-        <p>Me equivoqué de horario</p>
-      </div>
-      <div className="container-cancel">
-        <p className="p-cancel">Su reserva actual será cancelada</p>
-        <p className="p-cancel">La cancelación no puede ser revertida</p>
-        <button className="btn-primary" style={{ backgroundColor: "#E53939" }}>
-          <span className="button-cancel-text">Confirmar cancelación</span>
-        </button>
-      </div>
-      <hr className="line-hr" />
-      <div className="no-show">
-        <input type="checkbox" name="" value="" className="checkbox-noshow" />
-        <p>Encontré un lugar mejor</p>
-      </div>
-      <hr className="line-hr" />
-      <div className="no-show">
-        <input type="checkbox" name="" value="" className="checkbox-noshow" />
-        <p>Me cancelaron</p>
-      </div>
-      <hr className="line-hr" />
-      <div className="no-show" style={{ marginBottom: "122px" }}>
-        <input type="checkbox" name="" value="" className="checkbox-noshow" />
-        <p>Otro</p>
+      <div style={{ marginBottom: "50px" }}>
+        {checkboxes.map((checkbox) => {
+          return (
+            <div key={checkbox.id}>
+              <hr className="line-hr" />
+              <div className="no-show">
+                <input
+                  type="checkbox"
+                  checked={checkbox.isChecked}
+                  onChange={() => handleCheckboxChange(checkbox.id)}
+                  className="checkbox-noshow"
+                />
+                <p>{checkbox.text}</p>
+              </div>
+              {checkbox.isChecked && (
+                <div className="container-cancel">
+                  <p className="p-cancel">Su reserva actual será cancelada</p>
+                  <p className="p-cancel">
+                    La cancelación no puede ser revertida
+                  </p>
+                  <button
+                    className="btn-primary"
+                    style={{ backgroundColor: "#E53939" }}
+                  >
+                    <span className="button-cancel-text">
+                      Confirmar cancelación
+                    </span>
+                  </button>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     </>
   );
